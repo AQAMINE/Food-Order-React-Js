1a39b2a753a5d3df38da19aafdc051b607272ab8
amine aqebli <aqebli36@gmail.com>
2022-08-31 15:01:44 +0100

    Refs & Forward Refs

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,29 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = props => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+        const entredAmount = amountInputRef.current.value;
+        const entredAmountNumber = +entredAmount;
+
+        if(entredAmount.trim().length === 0 || entredAmountNumber < 1 || entredAmountNumber > 5){
+            setAmountIsValid(false);
+            return ;
+        }
+        props.onAddToCart(entredAmountNumber);
+    }
+
     return (
-        <form className={classes.form}>
-            <Input label="Amount" input={{
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input
+                ref={amountInputRef}
+                label="Amount" 
+                input={{
                 id: 'amount_' + props.id,
                 type: 'number',
                 min: '1',
@@ -13,6 +32,7 @@ const MealItemForm = props => {
                 defaultValue: '1'
             }}/>
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
         </form>
     );
 }
