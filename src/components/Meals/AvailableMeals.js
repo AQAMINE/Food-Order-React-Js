45f97e7a5172ff0a50248813a0a8eea5ca0653ec
amine aqebli <aqebli36@gmail.com>
2022-09-28 16:29:23 +0100

    Handling fetch errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,11 +7,15 @@ const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       setIsLoading(true);
       const response = await fetch('https://food-order-react-js-default-rtdb.firebaseio.com/meals.json');
+      if ( !response.ok ){
+        throw new Error('Something went wrong!');
+      }
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -25,14 +29,29 @@ const AvailableMeals = () => {
       }
       setMeals(loadedMeals);
       setIsLoading(false);
-    }
-    fetchMeals();
+    };
+    
+      fetchMeals().catch(error => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
+    
   }, []);
 
   if ( isLoading ) {
-    return <section className={classes.mealsLoading}>
-      <p>Loading...</p>
-    </section>
+    return ( 
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+        <section className={classes.MealsError}>
+          <p>{httpError}</p>
+      </section>
+    );
   }
 
   const mealsList = meals.map((meal) => (
