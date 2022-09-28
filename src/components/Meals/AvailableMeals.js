c53b7377008e4ca5a98f1e1838450c5975686017
amine aqebli <aqebli36@gmail.com>
2022-09-28 16:14:47 +0100

    Loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,8 +6,11 @@ import MealItem from "./MealItem/MealItem";
 const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
       const response = await fetch('https://food-order-react-js-default-rtdb.firebaseio.com/meals.json');
       const responseData = await response.json();
 
@@ -21,9 +24,16 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     }
     fetchMeals();
   }, []);
+
+  if ( isLoading ) {
+    return <section className={classes.mealsLoading}>
+      <p>Loading...</p>
+    </section>
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
